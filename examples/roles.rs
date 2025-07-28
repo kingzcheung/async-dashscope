f362a4ce840f4363ec92050803692e7b4e0fc744
kingzcheung <kingzcheung@gmail.com>
2025-07-28 11:44:41 +0800

    feat(operation): 为 generation 操作的消息类型提供专用 Builder
    - 新增 UserMessageBuilder、SystemMessageBuilder、ToolMessageBuilder 和 AssistantMessageBuilder
    - 这些 Builder 预设了消息类型的角色，简化了消息构建过程
    - 更新了 examples/roles.rs 示例，展示了新 Builder 的使用方法

--- examples/roles.rs
+++ examples/roles.rs
@@ -1,7 +1,7 @@
 use async_dashscope::{
     operation::{
         common::ParametersBuilder,
-        generation::{GenerationParamBuilder, InputBuilder, MessageBuilder},
+        generation::{GenerationParamBuilder, InputBuilder, MessageBuilder, UserMessageBuilder},
     },
     Client,
 };
@@ -9,13 +9,16 @@ use async_dashscope::{
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenvy::dotenv()?;
+    // let s = UserMessageBuilder::default().content("请将这句话翻译成英文：你好，世界！").build()?.into();
     let request = GenerationParamBuilder::default()
         .model("qwen-turbo".to_string())
         .input(
             InputBuilder::default()
                 .messages(vec![
                     MessageBuilder::default().role("system").content("你是一个出色的翻译专家").build()?,
-                    MessageBuilder::default().user().content("请将这句话翻译成英文：你好，世界！").build()?,
+                    // MessageBuilder::default().user().content("请将这句话翻译成英文：你好，世界！").build()?, 
+                    // 和上面等效
+                    UserMessageBuilder::default().content("请将这句话翻译成英文：你好，世界！").build()?.into(),
                 ])
                 .build()?,
         )
